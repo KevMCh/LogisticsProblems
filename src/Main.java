1e954a9f827fb4f40fa126618a2e85cf77d6f4a6
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2017-06-06 16:37:45 +0100

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 /**
  * Project of metaheuristics
- * 
+ *
  * MÃ¡ster Universitario en IngenierÃ­a InformÃ¡tica
  * E.S.I.T.â€“ INFORMÃ�Ì�TICA
  * Departamento de IngenierÃ­a InformÃ¡tica y de Sistemas
  * Sistemas Inteligentes e InteracciÃ³n Persona Computador
- * 
- * Project to study of metaheuristics, it is a higher-level procedure or heuristic 
+ *
+ * Project to study of metaheuristics, it is a higher-level procedure or heuristic
  * designed to find, generate, or select a heuristic (partial search algorithm) that
- * may provide a sufficiently good solution to an optimization problem, especially 
+ * may provide a sufficiently good solution to an optimization problem, especially
  * with incomplete or imperfect information or limited computation capacity.
- * 
+ *
  * Main class to ejecute the program.
  */
 
@@ -28,28 +28,11 @@ public class Main {
 	public static void main(String[] args) {
 		if(args.length == 1) {
 			Graph graph = new Graph(args[0]);
-			SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing (graph);
 			HillClimbing hillclimbing = new HillClimbing(graph);
-						
-			ArrayList<Double> results = new ArrayList<Double> ();
+
+			ArrayList<Integer> result = hillclimbing.solve();
 			
-			/* Hill Climbing (local search) */
-			@SuppressWarnings("unused")
-			ArrayList<Integer> solutionsHillClimbing = new ArrayList<Integer>();
-			solutionsHillClimbing = hillclimbing.solve();
-			hillclimbing.printResult();
-			
-			/* Simulated Annealing */
-			System.out.println("Simulated Annealing");
-			for(int i = 0; i < 100; i++) {
-				ArrayList<Integer> solutionSimulatedAnnealing =
-						simulatedAnnealing.solve();
-			
-				results.add(simulatedAnnealing.calculateCostTour(solutionSimulatedAnnealing));
-			}
-						
-			Statistics statistics = new Statistics(results);
-			statistics.showStatistics();
-	    }
+			System.out.println(result);
+		}
 	}
 }
