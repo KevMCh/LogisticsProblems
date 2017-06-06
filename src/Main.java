9c43de3b1cf804f570533e026a1a25079529477d
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2017-06-06 17:12:19 +0100

    Main test

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 /**
  * Project of metaheuristics
- *
+ * 
  * MÃ¡ster Universitario en IngenierÃ­a InformÃ¡tica
  * E.S.I.T.â€“ INFORMÃ�Ì�TICA
  * Departamento de IngenierÃ­a InformÃ¡tica y de Sistemas
  * Sistemas Inteligentes e InteracciÃ³n Persona Computador
- *
- * Project to study of metaheuristics, it is a higher-level procedure or heuristic
+ * 
+ * Project to study of metaheuristics, it is a higher-level procedure or heuristic 
  * designed to find, generate, or select a heuristic (partial search algorithm) that
- * may provide a sufficiently good solution to an optimization problem, especially
+ * may provide a sufficiently good solution to an optimization problem, especially 
  * with incomplete or imperfect information or limited computation capacity.
- *
+ * 
  * Main class to ejecute the program.
  */
 
@@ -27,12 +27,26 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		if(args.length == 1) {
-			Graph graph = new Graph(args[0]);
-			HillClimbing hillclimbing = new HillClimbing(graph);
-
-			ArrayList<Integer> result = hillclimbing.solve();
 			
-			System.out.println(result);
-		}
+			Graph graph = new Graph(args[0]);
+			
+			for(int i = 0; i < 3; i++) {
+				
+				ArrayList<Integer> solutions = new ArrayList<Integer>();
+				ArrayList<Double> results = new ArrayList<Double> ();
+				
+				for(int j = 0; j < 100; j++) {
+					
+					HillClimbing problem = new HillClimbing(graph);
+					solutions = problem.solve();
+	
+					results.add(problem.calculateCostTour(solutions));
+				}
+							
+				Statistics statistics = new Statistics(results);
+				statistics.showStatistics();
+				System.out.println();
+			}
+	    }
 	}
 }
